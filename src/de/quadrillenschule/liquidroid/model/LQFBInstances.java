c47f494994c6c8277265527a034e89174254e361
andizottmann <zottmann@quadrillenschule.de>
2012-03-13 00:39:10 +0100

    Berlin added

--- src/de/quadrillenschule/liquidroid/model/LQFBInstances.java
+++ src/de/quadrillenschule/liquidroid/model/LQFBInstances.java
@@ -16,7 +16,7 @@ public class LQFBInstances extends ArrayList<LQFBInstance> {
 
     ArrayList<LQFBInstance> instances;
     LiqoidApplication liqoidApplication;
-    public static boolean selectionUpdatesForRefresh=false;
+    public static boolean selectionUpdatesForRefresh = false;
 
     public LQFBInstances(LiqoidApplication liqoidApplication) {
         super();
@@ -28,33 +28,39 @@ public class LQFBInstances extends ArrayList<LQFBInstance> {
     private void initInstances() {
 
         if (isEmpty()) {
-             this.add(new LQFBInstance(liqoidApplication,"PP Bund","DE_PIRATEN_BUND",
+            this.add(new LQFBInstance(liqoidApplication, "PP Bund", "DE_PIRATEN_BUND",
                     "Piraten Bund",
                     "https://lqfb.piratenpartei.de/pp/api/",
                     "https://lqfb.piratenpartei.de/pp/",
                     "6Bw8HGL8Bp2z4wK6L3Zw", "1.x"));
-              this.add(new LQFBInstance(liqoidApplication,"PP MV","DE_PIRATEN_MV",
+            this.add(new LQFBInstance(liqoidApplication, "PP Berlin", "DE_PIRATEN_BE",
+                    "Piraten Berlin",
+                    "https://lqpp.de/be/api/",
+                    "https://lqpp.de/be/",
+                    "MjXNjRD3qSYbgjKrhYgC", "1.x"));
+
+            this.add(new LQFBInstance(liqoidApplication, "PP MV", "DE_PIRATEN_MV",
                     "Piraten Mecklenburg-Vorpommern",
                     "https://lqpp.de/mv/api/",
                     "https://lqpp.de/mv/",
                     "VpmyJGYbqTQPcc9wyzzk", "1.x"));
 
 
-            this.add(new LQFBInstance(liqoidApplication,"PP SA","DE_PIRATEN_SA",
+            this.add(new LQFBInstance(liqoidApplication, "PP SA", "DE_PIRATEN_SA",
                     "Piraten Sachsen-Anhalt",
                     "http://lqfb.piraten-lsa.de/lsa/api/",
                     "http://lqfb.piraten-lsa.de/lsa/",
                     "jXKWm5rFLQXQ8f6LMf92", "1.x"));
-         /*   this.add(new LQFBInstance(liqoidApplication,"TEST1","DE_TESTINSTANZ_VALID",
-                    "Testinstanz (valid URL)",
-                    "http://dev.liquidfeedback.org/test/api/",
-                    "http://dev.liquidfeedback.org/test/",
-                    "GTR8MjH6x98w6mztGB7J", "1.x"));
-           this.add(new LQFBInstance(liqoidApplication,"TEST2","DE_TESTINSTANZ_INVALID",
-                    "Testinstanz (invalid URL)",
-                    "https://lqfb.piratenptei.de/pp/api/",
-                    "https://lqfb.piratenptei.de/pp/",
-                    "6Bw8HGL8Bp2z4wzhL3Zw", "1.x"));*/
+            /*   this.add(new LQFBInstance(liqoidApplication,"TEST1","DE_TESTINSTANZ_VALID",
+            "Testinstanz (valid URL)",
+            "http://dev.liquidfeedback.org/test/api/",
+            "http://dev.liquidfeedback.org/test/",
+            "GTR8MjH6x98w6mztGB7J", "1.x"));
+            this.add(new LQFBInstance(liqoidApplication,"TEST2","DE_TESTINSTANZ_INVALID",
+            "Testinstanz (invalid URL)",
+            "https://lqfb.piratenptei.de/pp/api/",
+            "https://lqfb.piratenptei.de/pp/",
+            "6Bw8HGL8Bp2z4wzhL3Zw", "1.x"));*/
 
         }
     }
@@ -71,7 +77,6 @@ public class LQFBInstances extends ArrayList<LQFBInstance> {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt("selectedinstance", id);
         editor.commit();
-       
-    }
 
+    }
 }
