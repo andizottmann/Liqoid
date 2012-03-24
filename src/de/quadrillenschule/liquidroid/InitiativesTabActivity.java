fa3a1f5d7027e55141affb8898d0e7f985dfaba0
andizottmann <zottmann@quadrillenschule.de>
2012-03-24 21:56:38 +0100

    Pulltorefresh deactivatebar

--- src/de/quadrillenschule/liquidroid/InitiativesTabActivity.java
+++ src/de/quadrillenschule/liquidroid/InitiativesTabActivity.java
@@ -102,6 +102,7 @@ public class InitiativesTabActivity extends Activity implements RefreshInisListT
         filterList();
         sortList();
         final PullToRefreshListView listView = (PullToRefreshListView) findViewById(R.id.initiativenList);
+        listView.enabled = ((LiqoidApplication) getApplication()).getGlobalPreferences().getBoolean("pulltorefresh", true);
         listView.setTextPullToRefresh(getString(R.string.ptr_pull_to_refresh));
         listView.setTextReleaseToRefresh(getString(R.string.ptr_release_to_refresh));
         listView.setTextRefreshing(getString(R.string.ptr_refreshing));
